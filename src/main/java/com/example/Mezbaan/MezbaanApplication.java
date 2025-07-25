7bbc2d884dc76c0023d2a8aa95c2b757bc2305a2
ali <alijafar.rajani@gmail.com>
2025-07-25 21:39:45 +0500

    Initial commit

---
+++ src/main/java/com/example/Mezbaan/MezbaanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mezbaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MezbaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MezbaanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mezbaan/MezbaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mezbaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MezbaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
